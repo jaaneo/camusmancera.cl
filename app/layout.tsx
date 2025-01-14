a9639db8966c6f6ba72972cb2815fe1148c0c7a8
Alejandro Saez <jaaneo@gmail.com>
2025-01-13 21:33:25 -0300

    add <Analytics />

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import "./fontawesome";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
